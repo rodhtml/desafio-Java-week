6c903ef740f29295fb38c79581049a58f5197b3c
rodhtml <rodhtml0@gmail.com>
2023-03-31 21:44:49 -0300

    controller

--- src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
+++ src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
@@ -13,5 +13,36 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ProdutoController {
 
-    
+    private ProdutoController(ProdutoService produtoService){
+        this.produtoService = produtoService;
+    };
+
+    private ProdutoService produtoService;
+
+    /*Listar todos os produtos*/
+    @GetMapping("/produtos/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Produto> listarProdutos(){
+        return produtoService.listarProdutos();
+    }
+
+    /*Listar produtos pelo ID*/
+    @GetMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Produto> listarPorId(@PathVariable(value = "id") Long id){
+        return produtoService.buscarProdutoPeloId(id);
+    }
+
+    /*Atualizar Produto pelo ID*/
+    @PutMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Produto> atualizarPorId(@PathVariable(value="id")Long id,@RequestBody Produto produto){
+        return produtoService.atualizarProdutoPeloId(produto,id);
+    }
+
+    @DeleteMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deletarPorId(@PathVariable(value="id") Long id){
+        return produtoService.apagarPeloId(id);
+    }
 }
